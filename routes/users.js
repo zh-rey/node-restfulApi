8d31381c3654ecb505772f47d3d5ad75ed2072d1
Rey <zh_rey@163.com>
2019-04-13 18:32:45 +0800

    添加sql操作

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,75 @@
 var express = require('express');
 var router = express.Router();
+var userDAO = require('../sql/userDAO');
+var result = require('../model/result');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* list users */
+router.get('/', function(req, res) {
+  console.log('list users called');
+  userDAO.list(function (users) {
+      res.json(result.createResult(true, users));
+  });
+});
+
+/* get user */
+router.get('/:id', function(req, res) {
+  var id = req.params.id;
+  console.log('get user called, id: ' + id);
+  userDAO.getById(id, function (user) {
+      res.json(result.createResult(true, user));
+  });
+});
+
+/* delete user */
+router.delete('/:id', function (req, res) {
+  var id = req.params.id;
+  console.log('delete user called, id=' + id);
+  userDAO.deleteById(id, function (success) {
+      res.json(result.createResult(success, null));
+  });
+});
+
+/* add users */
+router.post('/', function (req, res) {
+  console.log('post users called');
+  var user = req.body;
+  console.log(user);
+  userDAO.add(user, function (success) {
+      var r =  result.createResult(success, null);
+      res.json(r);
+  });
+});
+
+/* update users */
+router.put('/:id', function (req, res) {
+  console.log('update users called');
+  var user = req.body;
+  user.id = req.params.id;
+  console.log(user);
+  userDAO.update(user, function (success) {
+      var r =  result.createResult(success, null);
+      res.json(r);
+  });
+});
+
+/* patch users */
+router.patch('/:id', function (req, res) {
+  console.log('patch users called');
+  userDAO.getById(req.params.id, function (user) {
+      var username = req.body.username;
+      if(username) {
+          user.username = username;
+      }
+      var password = req.body.password;
+      if(password) {
+          user.password = password;
+      }
+      console.log(user);
+      userDAO.update(user, function (success) {
+          var r =  result.createResult(success, null);
+          res.json(r);
+      });
+  });
 });
 
 module.exports = router;
---
+++ sql/userDAO.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql');
+const mysqlConf = require('../conf/mysqlConf');
+const userSqlMap = require('./userSqlMap');
+const pool = mysql.createPool(mysqlConf.mysql);
+
+module.exports = {
+    add: function (user, callback) {
+        pool.query(userSqlMap.add, [user.username, user.password], function (error, result) {
+            if (error) throw error;
+            callback(result.affectedRows > 0);
+        });
+    },
+    list: function (callback) {
+        pool.query(userSqlMap.list, function (error, result) {
+            if (error) throw error;
+            callback(result);
+        });
+    },
+    getById: function (id, callback) {
+        pool.query(userSqlMap.getById, id, function (error, result) {
+            if (error) throw error;
+            console.log(result[0]);
+            callback(result[0]);
+        });
+    },
+    deleteById: function (id, callback) {
+        pool.query(userSqlMap.deleteById, id, function (error, result) {
+            if (error) throw error;
+            callback(result.affectedRows > 0);
+        });
+    },
+    update: function (user, callback) {
+        pool.query(userSqlMap.update, [user.username, user.password, user.id], function (error, result) {
+            if (error) throw error;
+            callback(result.affectedRows > 0);
+        });
+    }
+};
